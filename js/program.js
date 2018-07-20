c86c806c7e1aa1f6b9a783a00c7fe77d7ba0a02a
sszolosi <szolosi.alex@gmail.com>
2018-07-20 13:49:39 +0300

    adaugat tipurile de variabile

--- js/program.js
+++ js/program.js
@@ -1,3 +1,26 @@
 function changeImage() {
   document.getElementById('image').src = "images/silly.png";
 }
+
+//numeric
+var a=5, b=5, sum=a+b;
+console.log("Suma a+b=",sum +". Variable type:"+ typeof sum);
+
+//string
+var animal1= "lion";
+var animal2="tiger";
+console.log("Why the " + animal1 +", not the " + animal2 + ", is king of the jungle?" + " Variable type:"+ typeof animal1);
+
+//boolean
+var a=10, b=5, sum=(a+b)>a;
+console.log(sum +". Variable type:"+ typeof sum);
+
+//null - object
+ var emptySomething=null;
+ console.log("Variable type: "+ typeof emptySomething);
+
+ //undefined
+ var justSomeVariable;
+ console.log("Variable type: "+ typeof justSomeVariable);
+
+//????? symbol ?????
