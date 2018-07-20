ea5bc4ca5bf6c631f36e559f8e610ae7e3b4d84e
sszolosi <szolosi.alex@gmail.com>
2018-07-20 17:13:34 +0300

    alte tipuri de variabile

--- js/program.js
+++ js/program.js
@@ -3,24 +3,55 @@ function changeImage() {
 }
 
 //numeric
-var a=5, b=5, sum=a+b;
-console.log("Suma a+b=",sum +". Variable type:"+ typeof sum);
+var a = 5,
+  b = 5,
+  sum = a + b;
+console.log("Suma a+b=", sum + ". Variable type:" + typeof sum);
 
 //string
-var animal1= "lion";
-var animal2="tiger";
-console.log("Why the " + animal1 +", not the " + animal2 + ", is king of the jungle?" + " Variable type:"+ typeof animal1);
+var animal1 = "lion";
+var animal2 = "tiger";
+console.log("Why the " + animal1 + ", not the " + animal2 + ", is king of the jungle?" + " Variable type:" + typeof animal1);
 
 //boolean
-var a=10, b=5, sum=(a+b)>a;
-console.log(sum +". Variable type:"+ typeof sum);
+var a = 10,
+  b = 5,
+  sum = (a + b) > a;
+console.log(sum + ". Variable type:" + typeof sum);
 
-//null - object
- var emptySomething=null;
- console.log("Variable type: "+ typeof emptySomething);
+//null
+var emptySomething = null;
+console.log("Variable type: " + typeof emptySomething);
 
- //undefined
- var justSomeVariable;
- console.log("Variable type: "+ typeof justSomeVariable);
+//undefined
+var justSomeVariable;
+console.log("Variable type: " + typeof justSomeVariable);
 
 //????? symbol ?????
+//---------------------
+
+//arrays
+var pens = ["green", "red", "blue", "yellow", "orange"]
+console.log(pens[4] + ". Variable type:" + typeof pens);
+console.log(pens);
+
+//objects
+var televizor = {
+  brand: "Samsung",
+  model: "UE49MU6202",
+  anFabricatie: 2018,
+  tipTv: "Smart",
+  tehnologie: "HDR 4K"
+};
+console.log(televizor.brand, televizor.tipTv, televizor.tehnologie);
+
+//-----------------------------------------------------------------------------------
+
+var televizor = function(brand, model, tipTv, tehnologie) {
+  this.brand = brand;
+  this.model = model;
+  this.tipTv = tipTv;
+  this.tehnologie = tehnologie;
+};
+samsung = new televizor('Samsung', 'UE49MU6202', 'Smart', '3D');
+console.log(samsung);
