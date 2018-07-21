722f13a7823293fa0b18d0457de02ed5e06a9540
sszolosi <szolosi.alex@gmail.com>
2018-07-21 09:52:13 +0300

    adaugat functiile de la task 014

--- js/program.js
+++ js/program.js
@@ -1,6 +1,12 @@
+//-----------------------------012--------------------------------
 function changeImage() {
   document.getElementById('image').src = "images/silly.png";
-}
+};
+
+
+//-----------------------------013--------------------------------
+
+
 
 //numeric
 var a = 5,
@@ -55,3 +61,74 @@ var televizor = function(brand, model, tipTv, tehnologie) {
 };
 samsung = new televizor('Samsung', 'UE49MU6202', 'Smart', '3D');
 console.log(samsung);
+
+
+//--------------------------------014-------------------------------------
+
+//functie care nu intoarce nimic
+function nothingToReturn() {};
+console.log(nothingToReturn());
+
+//functie care intoarce true
+var a = 100;
+var b = 20;
+
+function thisTrue() {
+  if (a > b) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(thisTrue());
+
+//functie care ia 1 variabila si o intoarce
+var name = "Zumzum";
+
+function returnOneVariable() {
+  return "Hello " + name + "!"
+};
+console.log(returnOneVariable());
+
+//functie care ia 2 variabile a si b si le intoarce suma
+a = 12;
+b = 10;
+
+function sumVariables() {
+  var c = a + b;
+  return c;
+};
+console.log(sumVariables());
+
+//functie care ia 2 variabile a si b si le intoarce ca si string impreuna
+nume = "Popescu";
+varsta = "32"
+
+function stringVariable() {
+  var persoana = "Nume:" + nume + ". Varsta:" + varsta + " ani";
+  return persoana;
+};
+console.log(stringVariable() + ". Variable type:" + typeof stringVariable());
+
+//functie care ia 1 variabila si intoarce tipul ei
+a = "matematica"
+b = 10
+
+function examen() {
+  return a;
+};
+console.log(examen() + ". Variable type:" + typeof examen());
+
+//functie care ia 1 variabila si intoarce toate numerele de la 0 la acea variabila
+var a = 10;
+var numbers = [];
+var i;
+
+function returnNumbers() {
+  for (i = 0; i < a; i++) {
+    numbers.push(i);
+  }
+  return numbers;
+}
+console.log(returnNumbers());
